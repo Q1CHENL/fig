be691b821b08f4303435a78c24f8ec9f253a6790
Qichen Liu <liuqichne@gmail.com>
2024-12-11 22:38:10 +0100

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="fig-editor",
-    version="1.0.1",
+    version="1.0.2",
     author="Qichen Liu",
     author_email="",
     description="A simple and usable GIF editor",
